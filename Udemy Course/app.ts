9e1f1d5e637b9d63825038a30f7f7b5faf04b2f3
Martighervas <ghervasmartin@gmail.com>
2024-01-17 12:44:09 +0100

    type tuple OK

--- Udemy Course/app.ts
+++ Udemy Course/app.ts
@@ -1,11 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string] //esto es una TUPLE (tupla). la diferencia es que se le especifica que su valor solo puede ser un [] que dentro contenga UN number y UN string. En este caso tuvimos que especificar los tipos a mano, porque sino typescript infiere que en vez de una tuple, es un array que puede contener numbers y strings.
+} = {
+// const person = {
     name: 'Martin',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
 }
 
 let favoriteActivities: string[];
