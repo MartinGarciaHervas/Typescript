8e1e4b2da13ccb7d5f1a4672d00b0a742ddd9687
Martighervas <ghervasmartin@gmail.com>
2024-01-17 14:38:01 +0100

    literal types

--- Udemy Course/app.ts
+++ Udemy Course/app.ts
@@ -12,5 +12,8 @@ const combinedAges = combine(30, 26);
 console.log(combinedAges);
 
 const combinedNames = combine('Max', 'Anna')
-console.log(combinedNames);
+console.log(combinedNames); 
 
+const literalType = 5 //esto es un literal type, porque al ser una const no puede cambiar, y ya se le esta diciendo que es 5. entonces su tipo es 5.
+
+let literalType2: "chau" | "hola" //aca pasa lo mismo, solo que yo le estoy diciendo que tiene que ser o un hola o un chau
