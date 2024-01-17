f25866f6964a3981f81482d01ed2e759acad9df3
Martighervas <ghervasmartin@gmail.com>
2024-01-17 13:05:13 +0100

    Union types

--- Udemy Course/app.js
+++ Udemy Course/app.js
@@ -1,31 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string] //esto es una TUPLE (tupla). la diferencia es que se le especifica que su valor solo puede ser un [] que dentro contenga UN number y UN string. En este caso tuvimos que especificar los tipos a mano, porque sino typescript infiere que en vez de una tuple, es un array que puede contener numbers y strings.
-// } = {
-// // const person = {
-//     name: 'Martin',
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// }
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {})); // esto le asigna un numero a cada label, admin=0, read_only=1, author=2.
-//Tambien se le puede asignar un numero al primer elemento, para que los siguientes sigan ese orden, por ejemplo que admin = 5, entonces read_only = 6, y author = 7. Pueden ser tambien strings y numbers, lo que se quiera.
-var person = {
-    name: 'Martin',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.AUTHOR
-};
-var favoriteActivities;
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
--- Udemy Course/app.ts
+++ Udemy Course/app.ts
@@ -1,30 +1,16 @@
-// const person: {
-//     name: string;
-//     age: number;
-//     hobbies: string[];
-//     role: [number, string] //esto es una TUPLE (tupla). la diferencia es que se le especifica que su valor solo puede ser un [] que dentro contenga UN number y UN string. En este caso tuvimos que especificar los tipos a mano, porque sino typescript infiere que en vez de una tuple, es un array que puede contener numbers y strings.
-// } = {
-// // const person = {
-//     name: 'Martin',
-//     age: 30,
-//     hobbies: ['Sports', 'Cooking'],
-//     role: [2, 'author']
-// }
-
-enum Role {ADMIN, READ_ONLY, AUTHOR} // esto le asigna un numero a cada label, admin=0, read_only=1, author=2.
-//Tambien se le puede asignar un numero al primer elemento, para que los siguientes sigan ese orden, por ejemplo que admin = 5, entonces read_only = 6, y author = 7. Pueden ser tambien strings y numbers, lo que se quiera.
-
-const person = {
-    name: 'Martin',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.AUTHOR
+function combine(input1: number | string, input2: number | string) {
+    let result;
+    if(typeof input1 === 'number' && typeof input2 === 'number'){
+        result = input1 + input2
+    } else {
+        result = input1.toString() + input2.toString(); //en el caso de que sea un number y un string
+    }
+    return result
 }
 
-let favoriteActivities: string[];
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-console.log(person);
+const combinedNames = combine('Max', 'Anna')
+console.log(combinedNames);
 
-for (const hobby of person.hobbies){
-    console.log(hobby.toUpperCase());
-}
