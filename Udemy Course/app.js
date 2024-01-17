27e35aa48fb11c1fff0a7a732906b066ef12aa6d
Martighervas <ghervasmartin@gmail.com>
2024-01-17 14:46:00 +0100

    Union and personalized types OK

--- Udemy Course/app.js
+++ Udemy Course/app.js
@@ -4,7 +4,7 @@ function combine(input1, input2) {
         result = input1 + input2;
     }
     else {
-        result = input1.toString() + input2.toString();
+        result = input1.toString() + input2.toString(); //en el caso de que sea un number y un string
     }
     return result;
 }
@@ -12,3 +12,5 @@ var combinedAges = combine(30, 26);
 console.log(combinedAges);
 var combinedNames = combine('Max', 'Anna');
 console.log(combinedNames);
+var literalType = 5; //esto es un literal type, porque al ser una const no puede cambiar, y ya se le esta diciendo que es 5. entonces su tipo es 5.
+var literalType2; //aca pasa lo mismo, solo que yo le estoy diciendo que tiene que ser o un hola o un chau.
