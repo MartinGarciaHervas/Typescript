28cdc57bfa27fda68cdaa97234de5355e9948f7e
Martighervas <ghervasmartin@gmail.com>
2024-01-18 12:43:55 +0100

    privaye keys and methods

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string; //Aca se ponen las keys que queres que tengan los objetos de estas clases
+    private employees: string[] = [] //al agregarle el private(solo en TS), esto obliga a los developers a usar los metodos que alteran a esta property, y no permitiendoles cambiarla de cuaoquier manera. asi te aseguras de que si o si pasen por el codigo que esta en el metodo. //* PRIVATE se puede usar tambien en metodos.
 
     constructor(n: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse.
         this.name = n
@@ -7,12 +8,23 @@ class Department {
 
     describe() { //asi se crean metodos dentro de la clase
         console.log('Department: ' + this.name);
-        
+    }
+
+    addEmployee(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('Accounting')
+const accounting = new Department('Accounting');
 
-console.log(accounting);
+accounting.addEmployee('Martin');
+accounting.addEmployee('Roberto');
 
-accounting.describe()
+// accounting.employee[2] = 'Anna' //* ahora esto da error porque employee esta en privado.
+
+accounting.printEmployeeInformation();
