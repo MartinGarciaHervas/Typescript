5394dae6458df71262afa557134cceacf5fc85d1
Martighervas <ghervasmartin@gmail.com>
2024-01-18 12:55:25 +0100

    Readonly modifier

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -2,7 +2,7 @@ class Department {
     // private name: string; //Aca se ponen las keys que queres que tengan los objetos de estas clases
     private employees: string[] = [] //al agregarle el private(solo en TS), esto obliga a los developers a usar los metodos que alteran a esta property, y no permitiendoles cambiarla de cuaoquier manera. asi te aseguras de que si o si pasen por el codigo que esta en el metodo. //* PRIVATE se puede usar tambien en metodos.
 
-    constructor(private id:string, public name: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse. //* las keys que se inicializen aqui, no hace falta que las inicialicemos antes, como name por ejemplo.
+    constructor(private readonly id:string, public name: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse. //* las keys que se inicializen aqui, no hace falta que las inicialicemos antes, como name por ejemplo.
         // this.name = n;
     }
 
