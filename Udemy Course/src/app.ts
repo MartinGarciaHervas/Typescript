98687c872d573422f59240ba5f0133b4ff0de5e5
Martighervas <ghervasmartin@gmail.com>
2024-01-18 13:21:15 +0100

    overriding & protected modifier

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -1,6 +1,6 @@
 class Department {
     // private name: string; //Aca se ponen las keys que queres que tengan los objetos de estas clases
-    private employees: string[] = [] //al agregarle el private(solo en TS), esto obliga a los developers a usar los metodos que alteran a esta property, y no permitiendoles cambiarla de cuaoquier manera. asi te aseguras de que si o si pasen por el codigo que esta en el metodo. //* PRIVATE se puede usar tambien en metodos.
+    protected employees: string[] = [] //al agregarle el private(solo en TS), esto obliga a los developers a usar los metodos que alteran a esta property, y no permitiendoles cambiarla de cuaoquier manera. asi te aseguras de que si o si pasen por el codigo que esta en el metodo. //* PRIVATE se puede usar tambien en metodos.
 
     constructor(private readonly id:string, public name: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse. //* las keys que se inicializen aqui, no hace falta que las inicialicemos antes, como name por ejemplo.
         // this.name = n;
@@ -33,6 +33,13 @@ class ITDepartment extends Department { //Al hacer esto, la clase ITDepartment a
 class AccountingDepartment extends Department {
     constructor(id: string, private reports: string[]){
         super(id, 'Accounting')
+    }
+
+    addEmployee(employee: string) { //podemos sobreescribir los metodos si queremos
+        if(employee === 'Max'){
+            return;
+        }
+        this.employees.push(employee) //en este caso, al ser employee PRIVATE, tampoco va a estar disponible para las clases que heredan a Department, es por eso que en vez de private, podemos usar PROTECTED. basicamente es igual, solo que permite que las clases que heredan, tambien tengan acceso a esa propiedad
     }
 
     addReport(text: string){
