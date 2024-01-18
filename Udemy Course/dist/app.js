0b1487b9c8c6403a1c10e6cd8debb1aee5e45b58
Martighervas <ghervasmartin@gmail.com>
2024-01-18 15:28:31 +0100

    Implementig interfaces to classes

--- Udemy Course/dist/app.js
+++ Udemy Course/dist/app.js
@@ -1,10 +1,13 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Martin',
-    age: 26,
+class Person {
+    constructor(n) {
+        this.age = 26;
+        this.name = n;
+    }
     greet(phrase) {
-        console.log(phrase + ' ' + this.name);
-    },
-};
+        console.log(phrase + ' ' + this.name + '. I"m ' + this.age + ' years old');
+    }
+}
+let user1;
+user1 = new Person('Martin');
 user1.greet('Hi there!, I am');
--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -1,19 +1,25 @@
-interface Person {
+interface Greetable { // Se diferencia de un custon type en que: a) solo se pueden determinar estructuras de objetos, Esto hace que sea mas claro que lo que estas definiendo es la estructura de un objeto y no cualquier otra cosa. b) 
     name: string;
-    age: number;
 
     greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable{ //Las interfaces se pueden implementar en clases, y pueden implementarse varias a la vez, lo que obliga a esa clase a cumplir con lo que la estructura de la interface.
+    name: string;
+    age = 26;
 
-user1 = {
-    name: 'Martin',
-    age: 26,
+    constructor(n:string) {
+        this.name = n
+    }
+    
     greet(phrase: string) {
-        console.log(phrase + ' ' + this.name);
-        
-    },
+        console.log(phrase + ' ' + this.name + '. I"m ' + this.age + ' years old');
+    }
+
 }
+
+let user1: Greetable;
+
+user1 = new Person('Martin')
 
 user1.greet('Hi there!, I am')
