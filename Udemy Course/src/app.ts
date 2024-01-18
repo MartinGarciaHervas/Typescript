9b64970e0ca57793f04c17d2ba37062ff111d6ad
Martighervas <ghervasmartin@gmail.com>
2024-01-18 12:34:10 +0100

    this

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -4,8 +4,15 @@ class Department {
     constructor(n: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse.
         this.name = n
     }
+
+    describe() { //asi se crean metodos dentro de la clase
+        console.log('Department: ' + this.name);
+        
+    }
 }
 
 const accounting = new Department('Accounting')
 
 console.log(accounting);
+
+accounting.describe()
