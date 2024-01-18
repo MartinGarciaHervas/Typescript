aac7c0d23a87be11280c6a1cdad54c10bf81b7d1
Martighervas <ghervasmartin@gmail.com>
2024-01-18 12:52:55 +0100

    private & public modifier

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -1,13 +1,13 @@
 class Department {
-    name: string; //Aca se ponen las keys que queres que tengan los objetos de estas clases
+    // private name: string; //Aca se ponen las keys que queres que tengan los objetos de estas clases
     private employees: string[] = [] //al agregarle el private(solo en TS), esto obliga a los developers a usar los metodos que alteran a esta property, y no permitiendoles cambiarla de cuaoquier manera. asi te aseguras de que si o si pasen por el codigo que esta en el metodo. //* PRIVATE se puede usar tambien en metodos.
 
-    constructor(n: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse.
-        this.name = n
+    constructor(private id:string, public name: string){ //esta funcion reservada se ejecuta cada vez que se crea un objeto de la clase, entonces es como algo que se ejecuta al inicializarse. //* las keys que se inicializen aqui, no hace falta que las inicialicemos antes, como name por ejemplo.
+        // this.name = n;
     }
 
     describe() { //asi se crean metodos dentro de la clase
-        console.log('Department: ' + this.name);
+        console.log(`Department (${this.id}): ${this.name}`);
     }
 
     addEmployee(employee: string){
@@ -20,7 +20,9 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('A1', 'Accounting');
+
+accounting.describe();
 
 accounting.addEmployee('Martin');
 accounting.addEmployee('Roberto');
