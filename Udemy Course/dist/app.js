915de82df1fd16ce2a0b04e1cd414313df0784b3
Martighervas <ghervasmartin@gmail.com>
2024-01-19 12:30:20 +0100

    interfaces OK

--- Udemy Course/dist/app.js
+++ Udemy Course/dist/app.js
@@ -1,13 +1,1 @@
 "use strict";
-class Person {
-    constructor(n) {
-        this.age = 26;
-        this.name = n;
-    }
-    greet(phrase) {
-        console.log(phrase + ' ' + this.name + '. I"m ' + this.age + ' years old');
-    }
-}
-let user1;
-user1 = new Person('Martin');
-user1.greet('Hi there!, I am');
