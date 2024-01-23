b0b7a0a1138ec8f51a7584dd7f433aba3383bfae
Martighervas <ghervasmartin@gmail.com>
2024-01-23 12:58:24 +0100

    Generics types

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -1,2 +1,4 @@
+// const names: Array = []; //Generic types son los types que necesitan informacion adicional, como por ejemplo los arrays. Si solo los nombramos como type Array, a typescript le va a faltar informacion sobre lo que va dentro del array, dificultandonos a nosotros mas tarde.
 
+const names: Array<string> = []; //En este caso le estamos indicando que es un array de strings, entonces ahora typescript ya nos puede ayudar a trabajr con el, ya que sabe que contiene strings.
 
