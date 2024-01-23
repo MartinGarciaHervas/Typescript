0f297318906a1d4a11eb25c94bef0108e388d975
Martighervas <ghervasmartin@gmail.com>
2024-01-23 13:04:00 +0100

    More Generics

--- Udemy Course/dist/app.js
+++ Udemy Course/dist/app.js
@@ -1,2 +1,10 @@
 "use strict";
 const names = [];
+const promise = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('This is done');
+    }, 2000);
+});
+promise.then(data => {
+    console.log(data.length);
+});
--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -2,3 +2,13 @@
 
 const names: Array<string> = []; //En este caso le estamos indicando que es un array de strings, entonces ahora typescript ya nos puede ayudar a trabajr con el, ya que sabe que contiene strings.
 
+const promise: Promise<string> = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve('This is done')
+    },2000)
+}) //Este es otro tipo de generic, si no le indicamos lo que la promise va a devolver, typescript no lo va a saber.
+
+promise.then(data => {
+    console.log(data.length)
+})
+
