025467f0e4f9426a50003957d8e121bda1c75345
Martighervas <ghervasmartin@gmail.com>
2024-01-23 18:03:05 +0100

    all the places for decorators

--- Udemy Course/src/app.ts
+++ Udemy Course/src/app.ts
@@ -40,9 +40,28 @@ console.log(pers1);
 
 function Log(target: any, propertyName: string | Symbol){
     console.log('Property decorator!');
-    console.log(target, propertyName);
-    
-    
+    console.log(target, propertyName);   
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number){
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
 }
 
 class Product {
@@ -50,6 +69,7 @@ class Product {
     title: string;
     private _price: number;
 
+    @Log2
     set price(val: number){
         if(val > 0){
             this._price = val;
@@ -63,7 +83,8 @@ class Product {
         this._price = p
     }
 
-    getPriceWithTax(tax: number){
+    @Log3
+    getPriceWithTax(@Log4 tax: number){
         return this._price * (1 + tax)
     }
 }
