d1f0622b0e00b54c10e8d8187afbd249c3da0a74
Martighervas <ghervasmartin@gmail.com>
2024-01-24 15:39:48 +0100

    Gathering the inputs information

--- Proyecto de prueba/prj-00-initial-starting-setup/src/app.ts
+++ Proyecto de prueba/prj-00-initial-starting-setup/src/app.ts
@@ -36,10 +36,39 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Invalid Input');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, Number(enteredPeople)]
+        }
+
+    }
+
+    private clearInputValue() {
+        this.titleInputElement.value = ''
+        this.descriptionInputElement.value = ''
+        this.peopleInputElement.value = ''
+    }
+
     @autobind //hace falta que le agreguemos este decorator, porque sino el 'this' va a hacer referencia a otra cosa diferente a la clase, y nosotros necesitamos que haga referencia a la clase.
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)){
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputValue();
+        }
     }
 
     private configure() {
